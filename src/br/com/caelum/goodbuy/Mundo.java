f45a5087d8b3d2f324e3b111597fc2237d6a40a9
TiagoVeri <tiagoverisjc@gmail.com>
2020-02-14 17:22:50 -0300

    testes para entendimento endpoing vRaptor

--- src/br/com/caelum/goodbuy/Mundo.java
+++ src/br/com/caelum/goodbuy/Mundo.java
@@ -1,12 +1,32 @@
 package br.com.caelum.goodbuy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.caelum.vraptor.Resource;
 
 @Resource
 public class Mundo {
 	
-	public void boasVindas() {
-		System.out.println("Olá mundo");
+	public String boasVindas() {
+		return "olá mundo!";
+	}
+	
+	public List<String> paises(){
+		List<String> result = new ArrayList<>();
+	    result.add("Brasil");
+	    result.add("Portugal");
+	    result.add("Japão");
+	    result.add("Canadá");
+	    result.add("Paraguai");
+	    return result;
 	}
 
+	
+	public Integer soma() {
+		int num1 = 3;
+		int num2 = 2;
+		Integer sum = num1 + num2;
+		return sum;
+	}
 }
---
+++ src/br/com/caelum/goodbuy/testes/MessagemTeste.java
@@ -0,0 +1,12 @@
+package br.com.caelum.goodbuy.testes;
+
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class MessagemTeste {
+
+	public void teste() {
+		System.out.println("Teste teste");
+	}
+
+}
